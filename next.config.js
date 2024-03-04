8d87ea5f41488be11909de0962d486a285cd9f27
immohika <mohi222@pm.me>
2024-03-04 15:44:04 +0330

    update next config

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ const nextConfig = {
 	reactStrictMode: true,
 	experimental: {
 		instrumentationHook: true,
+
+		// Prevent dependencies from the getting bundled
+		serverComponentsExternalPackages: ['@node-rs/argon2', '@node-rs/bcrypt'],
 	},
 	webpack: (config, { isServer }) => {
-		// Prevent dependencies from the getting bundled
-		config.externals.push('@node-rs/argon2', '@node-rs/bcrypt');
-
 		// check https://github.com/mswjs/examples/pull/101#discussion_r1462254400
 		if (isServer) {
 			if (Array.isArray(config.resolve.alias)) {
